fe8c4d7c8a1b1300e5e878e22360a6d6a4e7a6c0
ZainAli - Sadiq <125101944+ZainAli82@users.noreply.github.com>
2023-04-06 12:40:52 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import "./styles.css";
+import { connect, createLocalTracks, RoomEvent } from "livekit-client";
+
+document.getElementById("connect").addEventListener("click", (e) => {
+  join();
+});
+
+async function join() {
+  const t = document.getElementById("jwt").value;
+  console.log(t);
+
+  const url = "wss://linuxconnectiontest-15hwbf7p.livekit.cloud";
+
+  const room = await connect(url, t);
+
+  room.on(RoomEvent.TrackSubscribed, (track, publication, participant) => {
+    console.log("Participant Subscribed");
+    attachTrack(track, participant);
+  });
+
+  const tracks = await createLocalTracks({
+    audio: true,
+    video: true
+  });
+  for (let track of tracks) {
+    await room.localParticipant.publishTrack(track);
+
+    console.log("Publishing Track");
+    console.log(track);
+    if (track.kind === "video") {
+      const v = document.getElementById("us");
+      track.attach(v);
+    }
+  }
+}
+
+function attachTrack(track, participant) {
+  const v = document.getElementById("them");
+  track.attach(v);
+}
